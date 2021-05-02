1bacc911cea07469d46807b3f9bccfbd283901d9
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-02 11:18:02 +0700

    Values and Variables

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,6 +1,27 @@
 // Linking a JavaScript File
 let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10;
 console.log(40 + 8 + 23 - 10);
+
+// Values and Variables
+console.log('Jonas');
+console.log(23);
+
+let firstName = 'Matilda';  // camelCase notation
+
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let jonas_matilda = 'JM';
+let $function = 27;
+
+let person = 'Jonas';
+const PI = 3.14;
+
+let myFirstJob = 'Programmer';
+let myCurrentJob = 'Teacher';
+
+let job1 = 'programmer';
+let job2 = 'teacher';
+
+console.log(myFirstJob);
