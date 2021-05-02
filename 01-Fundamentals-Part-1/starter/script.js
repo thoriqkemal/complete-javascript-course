f474617fa30e91a9287d8169150cd2ea11f2b2b9
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-02 13:35:12 +0700

    Data Types

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,8 +1,11 @@
 // Linking a JavaScript File
+/**
 let js = 'amazing';
 console.log(40 + 8 + 23 - 10);
+ */
 
 // Values and Variables
+/**
 console.log('Jonas');
 console.log(23);
 
@@ -25,3 +28,26 @@ let job1 = 'programmer';
 let job2 = 'teacher';
 
 console.log(myFirstJob);
+ */
+
+// Data Types
+let javascriptIsFun = true;
+console.log(true);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+// console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
+
