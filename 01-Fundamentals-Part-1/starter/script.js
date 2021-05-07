7a3e541e5dc495161fb3862fc87d183a4d8206d8
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-08 05:13:34 +0700

    Basic Operators

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -54,6 +54,7 @@ console.log(typeof null);
  */
 
 // let, const and var
+/**
 let age = 30;
 age = 31;
 
@@ -66,3 +67,34 @@ job = 'teacher';
 
 lastName = 'Schmedtmann';   // never declare variable like this
 console.log(lastName);
+ */
+
+// Basic Operators
+// Mathematical Operators
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(ageJonas * 2, ageJonas / 10, 2 ** 3);
+// 2 ** 3 means 2 to the power of 3 = 2 * 2 * 2
+
+const firstName = 'Jonas';
+const lastName = 'Schmedtmann';
+console.log(firstName + ' ' + lastName);
+
+// Assignment Operators = Operator proceedings
+let x = 10 + 5; // 15
+x += 10;        // x = x + 10 => 25
+x *= 4;         // x = x * 4 => 100
+x++;            // x = x + 1
+x--;            // x = x - 1
+console.log(x);
+
+// Comparison Operators
+console.log(ageJonas > ageSarah);  // >, < , >= <=
+console.log(ageSarah >= 18);
+
+const isFullAge = ageSarah >= 18;
+
+console.log(now - 1991 > now - 2018);
