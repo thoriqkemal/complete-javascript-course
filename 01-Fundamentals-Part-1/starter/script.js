edd47ee6f216b6cbe1a6eebec5a890f3241ecda5
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-08 10:23:03 +0700

    Taking Decisions: if /else Statements

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -151,6 +151,7 @@ console.log(BMIMark, BMIJohn, markHigherBMI);
  */
 
 // Strings and Template Literals
+/**
 const firstName = 'Jonas';
 const job = 'Teacher';
 const birthYear = 1991;
@@ -170,3 +171,25 @@ lines');
 console.log(`String with
 multiple
 lines`);
+ */
+
+// Taking Decisions: if /else Statements
+const age = 15;
+
+// Control Structure
+if (age >= 18) {
+  console.log('Sarah can start driving license 🚙');
+} else {
+  const yearsLeft = 18 - age;
+  console.log(`Sarah is too young. Wait another ${yearsLeft} years :)`);
+}
+
+const birthYear = 1998;
+
+let century;
+if (birthYear <= 2000) {
+  century = 20;
+} else {
+  century = 21;
+}
+console.log(century);
