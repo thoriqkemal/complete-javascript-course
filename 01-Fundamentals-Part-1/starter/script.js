2c9ce4946233063aac674ceeaa6a7a09aa8dd9da
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-08 23:52:43 +0700

    Equality Operators: == vs. ===

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -264,7 +264,7 @@ console.log(n);
  */
 
 // Truthy and Falsy Values
-
+/**
 // 5 Falsy values: 0, '', undefined, null, NaN
 // Truthy values: empty object, any number that is not zero and any string that is not an empty string
 
@@ -287,3 +287,26 @@ if (height) {
 } else {
   console.log('Height is UNDEFINED');
 }
+ */
+
+// Equality Operators: == vs. ===
+const age = '18';
+if (age === 18) console.log('You became an adult :D (strict)');
+if (age == 18) console.log('You became an adult :D (loose)');
+
+const favourite = Number(prompt("What's your favourite number?"));
+console.log(favourite);
+console.log(typeof favourite);
+
+// loose equality always do type coercion
+if (favourite === 23) {  // '23' === 23
+  console.log('Cool! 23 is an amazing number!');
+} else if (favourite === 7) {
+  console.log('7 is also a cool number');
+} else if (favourite === 9) {
+  console.log('9 is also a cool number');
+} else {
+  console.log('Number is not 23 or 7 or 9');
+}
+
+if (favourite !== 23) console.log('Why not 23?');
