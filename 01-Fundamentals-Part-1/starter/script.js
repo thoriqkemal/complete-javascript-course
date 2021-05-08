52dfbf69c48949aace779f33b6a0c1e7c2ddd0ea
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-08 09:17:17 +0700

    Operator Precedence

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -71,6 +71,7 @@ console.log(lastName);
 
 // Basic Operators
 // Mathematical Operators
+/**
 const now = 2037;
 const ageJonas = now - 1991;
 const ageSarah = now - 2018;
@@ -98,3 +99,19 @@ console.log(ageSarah >= 18);
 const isFullAge = ageSarah >= 18;
 
 console.log(now - 1991 > now - 2018);
+ */
+
+// Operator Precedence
+const now = 2037;
+const ageJonas = now - 1991;
+const ageSarah = now - 2018;
+console.log(ageJonas, ageSarah);
+
+console.log(now - 1991 > now - 2018);
+
+let x, y;
+x = y = 25 - 10 - 5;  // x = y = 10, x = 10
+console.log(x, y);
+
+const averageAge = (ageJonas + ageSarah) / 2;
+console.log(ageJonas, ageSarah, averageAge);
