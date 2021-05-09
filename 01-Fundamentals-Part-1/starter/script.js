942f4e6320d3e1019086301c9a34cbd10e74ef44
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-09 10:31:45 +0700

    Statements and Expressions

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -393,6 +393,7 @@ if (scoreDolphins > scoreKoalas && scoreDolphins >= 100) {
  */
 
 // The switch Statement
+/**
 const day = 'thursday';
 
 switch (day) {
@@ -433,3 +434,19 @@ if (day === 'monday') {
 } else {
   console.log('Not a valid day!');
 }
+ */
+
+// Statements and Expressions
+// Expression is a piece of code that produces a value
+// 3 + 4
+// 1991
+// true && false && !false
+
+// Statement is a bigger piece of code that is executed and which does not produce a value on itself
+
+if (23 > 10) {
+  const str = '23 is bigger';
+}
+
+const me = 'Jonas';
+console.log(`I'm ${2037 - 1991} years old ${me}`);
