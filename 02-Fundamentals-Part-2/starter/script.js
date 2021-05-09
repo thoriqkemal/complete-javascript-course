c124d6d431905fd1f388f482acd2e9791166b1a2
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-09 16:55:01 +0700

    Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,7 @@
 // Activating Strict Mode
 'use strict';
 
+/*
 let hasDriversLicense = false;
 const passTest = true;
 
@@ -9,3 +10,27 @@ if (hasDriversLicense) console.log('I can drive :D');
 
 // const interface = 'Audio';
 // const private = 534;
+*/
+
+// Functions
+function logger() {
+  console.log('My name is Jonas');
+}
+
+// calling / running / invoking function
+logger();
+logger();
+logger();
+
+function fruitProcessor(apples, oranges) {
+  const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+  return juice;
+}
+
+const appleJuice = fruitProcessor(5, 0);
+console.log(appleJuice);
+
+const appleOrangeJuice = fruitProcessor(2, 4);
+console.log(appleOrangeJuice);
+
+Number('23');
