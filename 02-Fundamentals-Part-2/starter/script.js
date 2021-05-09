1eb55a7afa11d4d811f45fbfed57cb059afa199b
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-09 22:50:32 +0700

    Reviewing Functions

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -76,6 +76,7 @@ console.log(yearUntilRetirement(1980, 'Bob'));
  */
 
 // Functions Calling Other Functions
+/**
 function cutFruitPieces(fruit) {
   return fruit * 4;
 }
@@ -89,3 +90,25 @@ function fruitProcessor(apples, oranges) {
 }
 
 console.log(fruitProcessor(2, 3));
+ */
+
+// Reviewing Functions
+const calcAge = function (birthYear) {
+  return 2037 - birthYear;
+}
+
+const yearUntilRetirement = function (birthYear, firstName) {
+  const age = calcAge(birthYear);
+  const retirement = 65 - age;
+
+  if (retirement > 0) {
+    console.log(`${firstName} retires in ${retirement} years`);
+    return retirement;
+  } else {
+    console.log(`${firstName} has already retired 🎉`);
+    return -1;
+  }
+}
+
+console.log(yearUntilRetirement(1991, 'Jonas'));
+console.log(yearUntilRetirement(1950, 'Mike'));
