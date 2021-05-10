0c1f2cf90a2ac6f99e93bc0ebff02f8de6a8f550
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-10 12:08:08 +0700

    Dot vs. Bracket Notation

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -275,6 +275,7 @@ console.log(bills, tips, totals);
  */
 
 // Introduction to Objects
+/**
 const jonasArray = [
   'Jonas',
   'Schmedtmann',
@@ -290,3 +291,39 @@ const jonas = {
   job: 'teacher',
   friends: ['Michael', 'Peter', 'Steven']
 };
+ */
+
+// Dot vs. Bracket Notation
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  age: 2037 - 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven']
+};
+
+console.log(jonas);
+
+console.log(jonas.lastName);
+console.log(jonas['lastName']);
+
+const nameKey = 'Name';
+console.log(jonas['first' + nameKey]);
+console.log(jonas['last' + nameKey]);
+
+// const interestedIn = prompt('What do you want to know about Jonas? Choose between firstName, lastName, age, job and friends');
+
+// if (jonas[interestedIn]) {
+//   console.log(jonas[interestedIn]);
+// } else {
+//   console.log('Wrong request! Choose between firstName, lastName, age, job and friends');
+// }
+
+jonas.location = 'Portugal';
+jonas['twitter'] = '@jonasschmedtman';
+console.log(jonas);
+
+// Challenge
+// 'Jonas has 3 friends, and his best friend is called michael'
+
+console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
