71672ab4dfdf604b85f20c39325eac6302d3e964
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-12 22:32:49 +0700

    Object Methods

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -294,6 +294,7 @@ const jonas = {
  */
 
 // Dot vs. Bracket Notation
+/**
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
@@ -327,3 +328,40 @@ console.log(jonas);
 // 'Jonas has 3 friends, and his best friend is called michael'
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his best friend is called ${jonas.friends[0]}`);
+ */
+
+// Object Methods
+const jonas = {
+  firstName: 'Jonas',
+  lastName: 'Schmedtmann',
+  birthYear: 1991,
+  job: 'teacher',
+  friends: ['Michael', 'Peter', 'Steven'],
+  hasDriversLicense: false,
+
+  // calcAge: function (birthYear) {
+  //   return 2037 - birthYear;
+  // }
+
+  // calcAge: function () {
+  //   // console.log(this);
+  //   return 2037 - this.birthYear;
+  // }
+
+  calcAge: function () {
+    this.age = 2037 - this.birthYear;
+    return this.age;
+  },
+
+  getSummary: function () {
+    return `${this.firstName} is ${this.calcAge()}-year old ${this.job}, and he has ${this.hasDriversLicense ? 'a' : 'no'} driver's license`;
+  }
+};
+
+console.log(jonas.calcAge());
+console.log(jonas.age);
+
+// Challenge
+// "Jonas is a 46-year old teacher, and he has a/no driver's license"
+
+console.log(jonas.getSummary());
