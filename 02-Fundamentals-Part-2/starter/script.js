08b6f9e8f1328d075dc6db9c05064f809443e2ba
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-14 06:02:36 +0700

    The while Loop

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -472,6 +472,7 @@ for (let i = 0; i < jonas.length; i++) {
  */
 
 // Looping Backwards and Loops in Loops
+/**
 const jonas = [
   'Jonas',
   'Schmedtmann',
@@ -492,4 +493,26 @@ for (let exercise = 1; exercise < 4; exercise++) {
   for (let rep = 1; rep < 6; rep++) {
     console.log(`Exercise ${exercise} Lifting weight repetition ${rep} 🏋`);
   }
+}
+ */
+
+// The while Loop
+// for (let rep = 1; rep <= 10; rep++) {
+//   console.log(`Lifting weights repetition ${rep} 🏋`);
+// }
+
+let rep = 1;
+while (rep <= 10) {
+  // console.log(`WHILE: Lifting weights repetition ${rep} 🏋`);
+  rep++;
+}
+
+let dice = Math.trunc(Math.random() * 6) + 1;
+console.log(dice);
+
+while (dice !== 6) {
+  console.log(`You rolled a ${dice}`);
+  dice = Math.trunc(Math.random() * 6) + 1;
+
+  if (dice === 6) console.log('Loop is about to end...');
 }
