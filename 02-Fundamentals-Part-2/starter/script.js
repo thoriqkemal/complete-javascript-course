a02983d885f6d2ec7ab107d4f706d23d7381cc2f
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-13 12:40:37 +0700

    Coding Challenge #3

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -331,6 +331,7 @@ console.log(`${jonas.firstName} has ${jonas.friends.length} friends, and his bes
  */
 
 // Object Methods
+/**
 const jonas = {
   firstName: 'Jonas',
   lastName: 'Schmedtmann',
@@ -365,3 +366,45 @@ console.log(jonas.age);
 // "Jonas is a 46-year old teacher, and he has a/no driver's license"
 
 console.log(jonas.getSummary());
+ */
+
+// Coding Challenge #3
+/**
+Let's go back to Mark and John comparing their BMIs! This time, let's use objects to implement the calculations! Remember: BMI = mass / height ** 2 = mass / (height * height) (mass in kg and height in meter)
+
+Your tasks:
+1. For each of them, create an object with properties for their full name, mass, and height (Mark Miller and John Smith)
+2. Create a 'calcBMI' method on each object to calculate the BMI (the same method on both objects). Store the BMI value to a property, and also return it from the method
+3. Log to the console who has the higher BMI, together with the full name and the respective BMI. Example: "John's BMI (28.3) is higher than Mark's (23.9)!"
+
+Test data:
+Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+
+GOOD LUCK 😀
+ */
+
+const mark = {
+  fullName: 'Mark Miller',
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;  // 27.3
+    return this.BMI;
+  }
+}
+
+const john = {
+  fullName: 'John Smith',
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.BMI = this.mass / this.height ** 2;  // 24.2
+    return this.BMI;
+  }
+}
+
+if (mark.calcBMI() > john.calcBMI()) {
+  console.log(`${mark.fullName}'s BMI (${mark.calcBMI().toFixed(1)}) is higher than ${john.fullName}'s (${john.calcBMI().toFixed(1)})!`);
+} else if (mark.calcBMI() < john.calcBMI()) {
+  console.log(`${john.fullName}'s BMI (${john.calcBMI().toFixed(1)}) is higher than ${mark.fullName}'s (${mark.calcBMI().toFixed(1)})!`);
+}
