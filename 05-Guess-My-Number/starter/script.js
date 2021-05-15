3c9a8825f6601bae35c6c9318bd844fffd1dd119
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-15 18:32:13 +0700

    #3 Handling Click Events

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // PROJECT #1: Guess My Number!
+/**
 console.log(document.querySelector('.message').textContent);
 
 document.querySelector('.message').textContent = '🎉 Correct Number!';
@@ -11,3 +12,14 @@ document.querySelector('.score').textContent = 10;
 
 document.querySelector('.guess').value = 23;
 console.log(document.querySelector('.guess').value);
+ */
+
+// Handling Click Events
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = '⛔ No Number!';
+  }
+})
