7393097aa21c7f79f2cc9a3d7bb222e089010a0f
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-15 13:47:33 +0700

    #2 Selecting and Manipulating Elements

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,4 +1,13 @@
 'use strict';
 
-// PROJECT #1: Guess My NUmber!
+// PROJECT #1: Guess My Number!
 console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+// Selecting and Manipulating Elements
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 10;
+
+document.querySelector('.guess').value = 23;
+console.log(document.querySelector('.guess').value);
