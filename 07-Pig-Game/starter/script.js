e9d37e8b82969a17c073d229f5104e921fad1a03
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-16 12:18:52 +0700

    PROJECT #3: Pig Game #1

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,1 +1,12 @@
 'use strict';
+
+// PROJECT #3: Pig Game
+// Selecting Elements
+const score0El = document.querySelector('#score--0');
+const score1El = document.getElementById('score--1');
+const diceEl = document.querySelector('.dice');
+
+// Starting Conditions
+score0El.textContent = 0;
+score1El.textContent = 0;
+diceEl.classList.add('hidden');
