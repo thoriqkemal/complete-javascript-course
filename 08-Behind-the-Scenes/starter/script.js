0e6aa3a44519f73f72c624e5c32334cb7893c715
Thoriq Kemal <thoriqekemal@gmail.com>
2021-05-17 21:55:28 +0700

    Hoisting and TDZ

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,7 @@
 'use strict';
 
 // Scoping
+/**
 function calcAge(birthYear) {
   const age = 2037 - birthYear;
 
@@ -37,3 +38,49 @@ function calcAge(birthYear) {
 
 const firstName = 'Jonas';
 calcAge(1991);
+ */
+
+// Hoisting and TDZ
+
+// Variables
+console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991;
+
+// Functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+console.log(addArrow);
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+const addExpr = function (a, b) {
+  return a + b;
+}
+
+var addArrow = (a, b) => a + b;
+
+// Example
+console.log(numProducts);
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted!');
+}
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
+console.log(y === window.y);
+console.log(z === window.z);
